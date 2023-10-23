0af5f74b49557da1f97c77ff62ef6833dc91e712
Eric Cho <eric3cho@uw.edu>
2023-10-23 00:03:08 -0700

    changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
 export default function Home() {
   return (
-    <nav>
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="border-b border-gray-800 sticky top-0 bg-violet-300 text-gray-100 z-10">
       <div>
-        <a href="/">Eric Cho</a>
+        <h1 className="flex space-x-4">
+          Eric Cho
+        </h1>
+        <a href="/about" className="flex justify-end space-x-4 text-sm font-medium">
+          About
+        </a>
+        <a href="/career" className="space-x-4">
+          Career
+        </a>
+        <a href="/projects" className="">
+          Projects
+        </a>
       </div>
     </nav>
-  )
+  );
 }
